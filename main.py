8c54c02d191af7fd51a875d949eb11502f899d58
Chris Wright <cwright@richlandlibrary.com>
2026-08-01 12:59:02 -0400

    updating streamlit updates

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-import streamlit as st
-
 from dataclasses import dataclass, field
+from datetime import time
+
+import pandas as pd
+import streamlit as st
 
 
 @dataclass
 class Employee:
     name: str
     position: str
+
+    @property
+    def first_name(self) -> str:
+        return self.name.split()[0]
+
+    @property
+    def last_name(self) -> str:
+        return self.name.split()[1]
+
+    @property
+    def initials(self) -> str:
+        return "".join([i[0] for i in self.name.replace("-", " ").split()])
+
+    @property
+    def sharepoint(self) -> str:
+        return f"{self.first_name[0]}{self.last_name}"
 
 
 @dataclass
@@ -30,6 +48,18 @@ class Location:
         return len(self.employees) < self.min_staff
 
 
+@dataclass
+class TimeSlot:
+    start_time: int
+    end_time: int
+    locations: dict[str, Location] = field(default_factory=dict)
+
+    def get_time_label(self) -> str:
+        t1 = self.start_time - 12 if self.start_time > 12 else self.start_time
+        t2 = self.end_time - 12 if self.end_time > 12 else self.end_time
+        return f"{t1}-{t2}"
+
+
 def main():
     if "employees" not in st.session_state:
         st.session_state.employees = []
@@ -37,9 +67,14 @@ def main():
     if "locations" not in st.session_state:
         st.session_state.locations = []
 
+    if "time_slots" not in st.session_state:
+        st.session_state.time_slots = []
+
     st.title("Scheduler")
 
-    employee_tab, location_tab = st.tabs(["Employees", "Locations"])
+    employee_tab, location_tab, setup_hours_tab = st.tabs(
+        ["Employees", "Floor Locations", "Setup Hours"]
+    )
 
     with employee_tab:
         with st.form(border=True, key="employee_form"):
@@ -61,10 +96,20 @@ def main():
                 ],
             )
 
-            if st.form_submit_button("Add") and employee_position:
-                st.session_state.employees.append(
-                    Employee(name=employee_name, position=employee_position)
-                )
+            if st.form_submit_button("Add", type="primary"):
+                if employee_position:
+                    st.session_state.employees.append(
+                        Employee(name=employee_name, position=employee_position)
+                    )
+                    st.toast(
+                        f"Added {employee_name} ({employee_position})",
+                        icon=":material/person_add:",
+                    )
+                else:
+                    st.toast(
+                        f"Please add position for {employee_name}",
+                        icon=":material/person_alert:",
+                    )
 
         with st.popover("Employees"):
             for employee in st.session_state.employees:
@@ -83,18 +128,33 @@ def main():
                     "Minimum employees required", 0, 4
                 )
 
-            if (
-                st.form_submit_button("Add")
-                and location_name
-                and location_min_employees
-            ):
-                st.session_state.locations.append(
-                    Location(location_name, [], location_min_employees)
-                )
+            if st.form_submit_button("Add", type="primary"):
+                if location_name and location_min_employees:
+                    st.session_state.locations.append(
+                        Location(location_name, [], location_min_employees)
+                    )
+                elif not location_name:
+                    st.toast("Please enter a location name", icon=":material/error:")
 
         with st.popover("Locations"):
             for location in st.session_state.locations:
-                st.write(f"{location.name} ({', '.join(location.employees)} - {location.min_staff})")
+                st.write(
+                    f"{location.name} ({', '.join(location.employees)} - {location.min_staff})"
+                )
+
+    with setup_hours_tab:
+        with st.form(border=True, key="setup_hours_form"):
+            st.header("Setup hours")
+
+            hours_df = pd.DataFrame(
+                [{"weekday": "Sunday", "open": time(14, 0), "close": time(18, 0)}]
+            )
+
+            if st.form_submit_button("Save", type="primary"):
+                pass
+
+        with st.popover("Slots"):
+            pass
 
 
 if __name__ == "__main__":
